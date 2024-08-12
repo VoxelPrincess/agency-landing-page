30eae6f4444aa78ae4bbafde67be4edd3db95dd2
VoxelPrincess <anna.v.bekharskaya@gmail.com>
2024-08-12 23:19:30 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+(function () {
+  const toggle = document.querySelector(".menu-toggle");
+  const menu = document.querySelector(".menu");
+
+  toggle.addEventListener("click", function () {
+    menu.classList.toggle("shown");
+  });
+})();
